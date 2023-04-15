c96c38546839890a561c894bcbce8131ccf44713
akos borbely <macos.borbely@gmail.com>
2023-04-15 13:55:55 +0200

    click-handler-solved

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -2,16 +2,24 @@ import { GoLocation } from 'react-icons/go'
 import { BiSearchAlt } from 'react-icons/bi'
 import './Search.css'
 function Search(props) {
-
+    let result = null;
     function inputReader(event) {
         if (event.key === "Enter") return props.setCity(event.target.value)
+    }
+
+    function reader(event) {
+        return result = event.target.value
+    }
+
+    function clickHandler() {
+        return props.setCity(result)
     }
 
     return (
         <div id="search-container">
             <GoLocation size={50} />
-            <input onKeyDown={inputReader} id="city-input" type="text" placeholder="City..." />
-            <button id="search-button"><BiSearchAlt size={35} color='white' /></button>
+            <input onChange={reader} onKeyDown={inputReader} id="city-input" type="text" placeholder="City..." />
+            <button onClick={clickHandler} id="search-button"><BiSearchAlt size={35} color='white' /></button>
         </div>
     )
 }
